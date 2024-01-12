8be767c9ffe8ea835be83055d330cde1f96b2ebc
Anha <binteabdulhameed01@gmail.com>
2024-01-13 01:16:55 +0500

    feat:added second layout

--- index.js
+++ index.js
@@ -9,13 +9,56 @@
 // }
 
 document.addEventListener("DOMContentLoaded", function () {
-  changeTab(document.querySelector('.navIcon.active'));
+  changeTab(document.querySelector(".navIcon.active"));
 });
 
 function changeTab(clickedTab) {
-  var tabs = document.querySelectorAll('.navIcon');
+  var tabs = document.querySelectorAll(".navIcon");
   tabs.forEach(function (tab) {
-      tab.classList.remove('active');
+    tab.classList.remove("active");
   });
-  clickedTab.classList.add('active');
+  clickedTab.classList.add("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("custom-stacked-chart").getContext("2d");
+
+  const data = {
+    labels: ["20", "", "25", "", "30", "", "35", "", "40", "", "60", "", "70"],
+    datasets: [
+      {
+        label: "Employer",
+        backgroundColor: "#00039C",
+        data: [30, 50, 20],
+      },
+      {
+        label: "Employee",
+        backgroundColor: "#433AFB",
+        data: [10, 40, 30],
+      },
+      {
+        label: "Total interest",
+        backgroundColor: "#55B2FF",
+        data: [20, 10, 50],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: { stacked: true },
+      y: {
+        stacked: true,
+        ticks: {
+          beginAtZero: true, 
+        },
+      },
+    },
+  };
+
+  const stackedChart = new Chart(ctx, {
+    type: "bar",
+    data: data,
+    options: options,
+  });
+});
