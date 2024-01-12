5d7ddede4c20b27bc1a7537d63d4e50055ecaf10
Anha <binteabdulhameed01@gmail.com>
2024-01-12 12:55:58 +0500

    added nav

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+// // Update the current slider value (each time you drag the slider handle)
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  changeTab(document.querySelector('.navIcon.active'));
+});
+
+function changeTab(clickedTab) {
+  var tabs = document.querySelectorAll('.navIcon');
+  tabs.forEach(function (tab) {
+      tab.classList.remove('active');
+  });
+  clickedTab.classList.add('active');
 }
