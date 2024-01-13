c196d7ae8ea992c212360903903fa95f04efe06b
Anha <binteabdulhameed01@gmail.com>
2024-01-14 00:21:54 +0500

    made design responsive to mobiles

--- index.js
+++ index.js
@@ -9,56 +9,105 @@
 // }
 
 document.addEventListener("DOMContentLoaded", function () {
-  changeTab(document.querySelector(".navIcon.active"));
-});
-
-function changeTab(clickedTab) {
-  var tabs = document.querySelectorAll(".navIcon");
-  tabs.forEach(function (tab) {
-    tab.classList.remove("active");
-  });
-  clickedTab.classList.add("active");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const ctx = document.getElementById("custom-stacked-chart").getContext("2d");
-
-  const data = {
-    labels: ["20", "", "25", "", "30", "", "35", "", "40", "", "60", "", "70"],
+  var data = {
+    labels: ["20", "", "25", "", "30", "", "35", "", "40", "", "60", "", "65"],
     datasets: [
       {
-        label: "Employer",
-        backgroundColor: "#00039C",
-        data: [30, 50, 20],
+        backgroundColor: "#0001a2",
+        data: [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70],
+        barThickness: 20,
+        borderWidth: 1,
+        borderSkipped: true,
       },
       {
-        label: "Employee",
-        backgroundColor: "#433AFB",
-        data: [10, 40, 30],
+        label: "Dataset 2",
+        backgroundColor: "#493afd",
+        data: [12, 17, 22, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72],
+        barThickness: 20,
+        borderWidth: 0,
       },
       {
-        label: "Total interest",
-        backgroundColor: "#55B2FF",
-        data: [20, 10, 50],
+        label: "Dataset 3",
+        backgroundColor: "#73b0ff",
+        data: [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
+        barThickness: 20,
+        borderWidth: 0,
       },
     ],
   };
-
-  const options = {
+  var options = {
     scales: {
-      x: { stacked: true },
+      x: {
+        stacked: true,
+      },
       y: {
         stacked: true,
-        ticks: {
-          beginAtZero: true, 
-        },
       },
     },
   };
-
-  const stackedChart = new Chart(ctx, {
+  var ctx = document.getElementById("stackedBarChart").getContext("2d");
+  var myChart = new Chart(ctx, {
     type: "bar",
     data: data,
     options: options,
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const indicators = [
+    {
+      circularProgress: "average-Progress",
+      progressValue: "average-value",
+      progressEndValue: 78,
+    },
+    {
+      circularProgress: "top-progress",
+      progressValue: "top-value",
+      progressEndValue: 95,
+    },
+    {
+      circularProgress: "mine-progress",
+      progressValue: "mine-value",
+      progressEndValue: 59,
+    },
+  ];
+
+  function animateProgress(
+    circularProgressId,
+    progressValueId,
+    progressEndValue
+  ) {
+    let circularProgress = document.getElementById(circularProgressId);
+    let progressValue = document.getElementById(progressValueId);
+    let progressStartValue = 0;
+    let speed = 10;
+
+    let progress = setInterval(() => {
+      progressStartValue++;
+
+      progressValue.textContent = `${progressStartValue}%`;
+      circularProgress.style.background = `conic-gradient(#00D9AF ${
+        progressStartValue * 3.6
+      }deg, #ededed 0deg)`;
+
+      if (progressStartValue === progressEndValue) {
+        clearInterval(progress);
+      }
+    }, speed);
+  }
+
+  indicators.forEach((indicator) => {
+    animateProgress(
+      indicator.circularProgress,
+      indicator.progressValue,
+      indicator.progressEndValue
+    );
+  });
+});
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
